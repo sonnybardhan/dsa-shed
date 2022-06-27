10ffbdfdbb739134da1b05b0059d8f19c4fa3a3c
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-06-28 01:25:03 +0530

    rename files and update fibonacci with meoization

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -1,6 +1,26 @@
-function fib(n) {
-  if (n <= 1) return n;
-  return fib(n - 1) + fib(n - 2);
+// function fib(n) {
+//   if (n <= 1) return n;
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// console.log(fib(7));
+
+function fib() {
+  cache = {};
+
+  return function fib_inner(n) {
+    if (n <= 1) return n;
+
+    if (cache[n]) {
+      return cache[n];
+    }
+
+    cache[n] = fib_inner(n - 1) + fib_inner(n - 2);
+
+    return cache[n];
+  };
 }
 
-console.log(fib(7));
+const inner = fib();
+
+console.log(inner(6));
