aa12443cdcf5181d5e1e176ed5fda3c8d00c9629
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-06-28 16:03:46 +0530

    add strings folder and complete typedOut problem

---
+++ Strings/typedOut.js
@@ -0,0 +1,27 @@
+function typedOut(s, t) {
+  return buildString(s) === buildString(t);
+}
+
+function buildString(str) {
+  let temp = [];
+
+  for (let char of str) {
+    if (char !== '#') {
+      temp.push(char);
+    } else {
+      temp.pop();
+    }
+  }
+  return temp.join('');
+}
+
+/**
+ * s: "ab#z"		t: "az#z" 	true
+ * s: "abc#d"		t: "acc#c" 	false
+ * s: "x#y#z#"	t: "a#" 		true
+ * s: "a###b"		t: "b" 			true
+ * s: "ab#z"		t: "Az#z" 	false
+ *
+ */
+console.log(typedOut('ab#z', 'az#z'));
+console.log(typedOut('a#z', 'az#z'));
