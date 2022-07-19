16d6965f637ca8d47b3b7d2a4e06a3b718e20f65
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-07-19 10:50:40 +0530

    level order

--- Binary Trees/max-depth.js
+++ Binary Trees/max-depth.js
@@ -29,4 +29,36 @@ class BinaryTree {
 
     return Math.max(leftCount, rightCount);
   }
+
+  levelOrder(node) {
+    if (!node) return [[]];
+    const q = [node];
+    const order = [];
+
+    let currLevel = [];
+    let childrenRemaining = q.length;
+
+    while (q.length) {
+      const currNode = q.shift();
+
+      currLevel.push(currNode.value);
+
+      if (currNode.left) {
+        q.push(currNode.left);
+      }
+
+      if (currNode.right) {
+        q.push(currNode.right);
+      }
+
+      childrenRemaining--;
+
+      if (childrenRemaining === 0) {
+        childrenRemaining = q.length;
+        order.push(currLevel);
+        currLevel = [];
+      }
+    }
+    return order;
+  }
 }
