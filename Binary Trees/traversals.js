587625f4bce7225eea4b4f6163d2b055ca791345
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-07-22 12:45:25 +0530

    attempt binary tree validation, full complete nodes

--- Binary Trees/traversals.js
+++ Binary Trees/traversals.js
@@ -167,21 +167,39 @@ class BinaryTree {
     return view;
   }
 
-  rightSideView_DFS(node, view = [], level = 0) {
-    //preorder NRL (adjusted, swapping R with L)
+  // rightSideView_DFS(node, view = [], level = 0) {
+  //   //preorder NRL (adjusted, swapping R with L)
 
+  //   if (!node) return view;
+
+  //   if (level >= view.length) {
+  //     view.push(node.val);
+  //   }
+
+  //   if (node.right) {
+  //     this.rightSideView_DFS(node.right, view, level + 1);
+  //   }
+
+  //   if (node.left) {
+  //     this.rightSideView_DFS(node.left, view, level + 1);
+  //   }
+
+  //   return view;
+  // }
+
+  rhs_DFS(node, view = [], level = 0) {
     if (!node) return view;
 
     if (level >= view.length) {
-      view.push(node.val);
+      view.push(node.value);
     }
 
     if (node.right) {
-      this.rightSideView_DFS(node.right, view, level + 1);
+      this.rhs_DFS(node.right, view, level + 1);
     }
 
     if (node.left) {
-      this.rightSideView_DFS(node.left, view, level + 1);
+      this.rhs_DFS(node.left, view, level + 1);
     }
 
     return view;
@@ -215,5 +233,116 @@ class BinaryTree {
     }
 
     return view;
+  }
+
+  // nodesInCompleteTree(node, count = 0, level = 0) {
+  //   //not sure
+  //   if (!node) return count;
+
+  //   count += Math.ceil(level / 2);
+
+  //   if (node.left) {
+  //     count += this.nodesInCompleteTree(node.left, count, level + 1);
+  //   }
+
+  //   if (node.right) {
+  //     count += this.nodesInCompleteTree(node.right, count, level + 1);
+  //   }
+
+  //   return count;
+  // }
+
+  nodesInCompleteTree(node, count = 0, level = 0) {
+    if (!node) return 0;
+
+    if (level > 0) {
+      const numNodes = 2 ** level - 1;
+
+      //nodes till the previous level
+      count += 2 * (numNodes - 1);
+
+      //adding current node;
+      count += 1;
+    }
+  }
+
+  binarySearch(arr, left, right, target) {
+    if (!arr.length) return -1;
+
+    let mid;
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+
+    while (left <= mid) {
+      mid = Math.floor((left + right) / 2);
+      if (arr[mid] === target) {
+        return arr[mid];
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+
+  bSearchRightMostNode(arr, left, right) {
+    if (!arr.length) return -1;
+
+    let mid;
+
+    while (left < right) {
+      mid = Math.ceil((left + right) / 2);
+      if (arr[mid] == null) {
+        right = mid - 1;
+      } else {
+        //arr[mid] != null
+        left = mid;
+      }
+    }
+    return arr[left]; //or arr[right] ... same thing
+  }
+
+  getTreeHeight(root) {
+    if (!root) return 0;
+
+    return 1 + this.getTreeHeight(root.left);
+  }
+
+  isValidBinaryTree(node) {
+    if (!node) return false;
+
+    if (node.left && node.right) {
+      if (node.left.value > node.value) {
+        return false;
+      }
+      if (node.right.value < node.value) {
+        return false;
+      }
+      if (node.left.value > node.right.value) {
+        return false;
+      }
+      if (node.left.value > node.right.value) {
+        return false;
+      }
+
+      const isLeftValid = this.isValidBinaryTree(node.left);
+      const isRightValid = this.isValidBinaryTree(node.right);
+
+      return isLeftValid && isRightValid;
+    } else if (node.left) {
+      if (node.left.value > node.value) {
+        return false;
+      }
+      return this.isValidBinaryTree(node.left);
+    } else if (node.right) {
+      if (node.right.value < node.value) {
+        return false;
+      }
+      return this.isValidBinaryTree(node.right);
+    }
+
+    //failing - [5, 4, 6, null, null, 3, 7];
+    return true;
   }
 }
